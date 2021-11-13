bf33c1a6e7a24988dc2c2f0e6095f438e01579b2
ashfana <ashfana@DESKTOP-NPR6MTA>
2021-11-13 11:16:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name_Address
+{
+    class nameaddress
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("ASHFANA SHAIKH\nVishal nagar Vijayapura");
+         
+        }
+    }
+}
